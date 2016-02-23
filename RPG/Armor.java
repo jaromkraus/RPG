d0756dc61f9998cfb1696c135e78b7a63544a2f7
jaromkraus <jaromkraus@gmail.com>
2016-02-23 07:49:06 -0800

    Armor Class changes
    It compiles now.

--- RPG/Armor.java
+++ RPG/Armor.java
@@ -6,15 +6,15 @@
  * @version 2.22.16
  */
 public abstract  class Armor extends Item implements DefenseCommands{
-//     boolean blocked(){
-//         
-//     }
+    boolean blocked(){
+        return true;
+  }
 String name;
 public Armor (String Name){
     name = Name;
-}
+  }
 public String getName(){
     return name;
-}
+  }
 //    
 }
